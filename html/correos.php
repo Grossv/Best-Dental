781e948e5acb62c44e522261a2ec11acd29eb1ed
Grossv <vilchezharvey747@gmail.com>
2020-10-19 16:57:29 -0600

    Solucion de error el el codigo de php. ERROR DE NOVATO XD

--- html/correos.php
+++ html/correos.php
@@ -2,7 +2,7 @@
     $destinatario = 'bestdental.supp@gmail.com';
     //Esto es al correo que se le enviara el mensaje
 
-    $nombres = $_POST['nombre'];
+    $nombres = $_POST['nombres'];
     $apellidos = $_POST['apellidos'];
     $telefono = $_POST['telefono'];
     $email = $_POST['email'];
@@ -13,11 +13,24 @@
     $razonCita = $_POST['razonCita'];
     $comentarios = $_POST['comentarios'];
 
-    $header = "Formulario (COMPLETO)";
-    $header. = "X-Mailer: PHP/". phpversion();
+    $header = 'From: ' .$email. "\r\n";
+    $header .= "X-Mailer: PHP/". phpversion() . "\r\n";
+    $header .= "Mime-Version: 1.0 \r\n";
+    $header .= "Content-Type: text-plain";
 
-    $mensajeCompleto = $razonCita . "\nAtentamente: " .$nombres;
+    $mensajeCompleto = "Este mensaje fue enviado por: " .$nombres .$apellidos . "\r\n";
+    $mensajeCompleto .= "Su email es: " .$email . "\r\n";
+    $mensajeCompleto .= "Telefono de contacto: " .$telefono . "\r\n";
+    $mensajeCompleto .= "Su primera Fecha agendada es el: " .$primeraFecha . "\r\n";
+    $mensajeCompleto .= "Su segunda Fecha agendada es el: " .$segundaFecha . "\r\n";
+    $mensajeCompleto .= "Su tercera Fecha agendada es el: " .$terceraFecha . "\r\n";
+    $mensajeCompleto .= "Agendo cita debido a que experimenta: " .$razonCita . "\r\n";
+    $mensajeCompleto .= "Comentarios o preguntas de este paciente: " .$comentarios . "\r\n";
+    $mensajeCompleto .= "Enviado el: " .date('d/m/Y', time());
 
-    $mail = @mail($destinatario, $email, $mensajeCompleto, $header);
-    echo "<script>alert('Cita agendada exitosamente')</script>";
-    echo "<script> setTimeout(\"location.href='formulario.php'\"1000)</script>";
+    $asunto = 'Cita Agendada';
+
+    mail($destinatario, $asunto, utf8_decode($mensajeCompleto), $header);
+
+    header("Location:../index.html");
+?>
