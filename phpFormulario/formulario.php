0bcd2b75969d64720d3f45707d1964570141c845
Grossv <vilchezharvey747@gmail.com>
2020-10-20 00:06:31 -0600

    Arrglando todos las confuciones de archivos creados en el intento de lograr una conexion con php exitosa.... Estado de la mision:Inconclusa

--- phpFormulario/formulario.php
+++ phpFormulario/formulario.php
@@ -36,7 +36,7 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda Best Dental</title>
-    <link rel="stylesheet" href="css/estiloFormulario.css">
+    <link rel="stylesheet" type="text/css" href="../css/estiloFormulario.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
 </head>
 <body>
